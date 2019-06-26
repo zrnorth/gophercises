afdd08e98034ccbb6599b64677c4bbac8c8f41aa
Zach North <zrnorth@gmail.com>
2019-06-26 01:35:14 -0700

    Problem 10, added scoring and basic dealer AI

--- gopher-10/main.go
+++ gopher-10/main.go
@@ -17,6 +17,40 @@ func (h Hand) String() string {
 	return strings.Join(strs, ", ")
 }
 
+// In blackjack the dealer only shows one card
+func (h Hand) DealerString() string {
+	return h[0].String() + ", **HIDDEN**"
+}
+
+func (h Hand) Score() int {
+	minScore := h.MinScore()
+	if minScore > 11 {
+		return minScore
+	}
+	for _, c := range h {
+		if c.Rank == deck.Ace {
+			return minScore + 10 // A goes from 1->11
+		}
+	}
+	return minScore
+}
+
+func (h Hand) MinScore() int {
+	score := 0
+	for _, c := range h {
+		score += min(int(c.Rank), 10) // cards can not be worth more than 10 in blackjack
+	}
+	return score
+}
+
+// helper function to get the min of two integers
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func draw(shoe []deck.Card) (deck.Card, []deck.Card) {
 	return shoe[0], shoe[1:]
 }
@@ -33,6 +67,42 @@ func main() {
 			*hand = append(*hand, card)
 		}
 	}
-	fmt.Println("Player:", player)
-	fmt.Println("Dealer:", dealer)
+
+	// Player action
+	var input string
+	for input != "s" { // While the player has not yet chosen to Stand
+		fmt.Println("Player:", player)
+		fmt.Println("Dealer:", dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, shoe = draw(shoe)
+			player = append(player, card)
+		}
+	}
+
+	// Dealer action
+	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
+		card, shoe = draw(shoe)
+		dealer = append(dealer, card)
+		fmt.Println("Dealer hits:", card)
+	}
+
+	pScore, dScore := player.Score(), dealer.Score()
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player:", player, "\nScore:", pScore)
+	fmt.Println("Dealer:", dealer, "\nScore:", dScore)
+	switch {
+	case pScore > 21:
+		fmt.Println("You busted.")
+	case dScore > 21:
+		fmt.Println("The dealer busted!")
+	case pScore > dScore:
+		fmt.Println("You win!")
+	case pScore < dScore:
+		fmt.Println("You lose.")
+	case pScore == dScore:
+		fmt.Println("Draw!")
+	}
 }
