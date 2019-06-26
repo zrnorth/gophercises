8ee80bb309aaab08b3151dd2bed86d67f2afa3b6
Zach North <zrnorth@gmail.com>
2019-06-25 18:14:31 -0700

    problem 3, rendering html

--- gopher-3/cmd/cyoaweb/main.go
+++ gopher-3/cmd/cyoaweb/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	cyoa "github.com/zrnorth/gopher/gopher-3"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "port to start the webapp on")
 	filename := flag.String("file", "story.json", "the json with the choose-your-own-adventure story")
 	flag.Parse()
 	fmt.Printf("Using the story %s.\n", *filename)
@@ -18,12 +20,12 @@ func main() {
 	if err != nil {
 		panic(err) // Shouldn't do this really but whatever
 	}
-
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.JSONStory(f)
+	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port %d", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- gopher-3/story.go
+++ gopher-3/story.go
@@ -1,5 +1,67 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"text/template"
+)
+
+func init() {
+	tmpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
+}
+
+var tmpl *template.Template
+
+var defaultHandlerTemplate = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+      <p>{{.}}</p>
+    {{end}}
+    <ul>
+      {{range .Options}}
+        <li>
+          <a href="/{{.Arc}}">{{.Text}}</a>
+        </li>
+      {{end}}
+    </ul>
+  </body>
+</html>
+`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Start with the intro story
+	err := tmpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Helper to read in stories
+func JSONStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
 // Story : self-explanatory
 type Story map[string]Arc
 
