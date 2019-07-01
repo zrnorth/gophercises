10a461ac44b7a088d034c03daf338b2efbace079
Zach North <zrnorth@gmail.com>
2019-07-01 14:01:37 -0700

    Problem 12, added the rename logic based on file counts

--- gopher-12/main.go
+++ gopher-12/main.go
@@ -2,24 +2,55 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"io/ioutil"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	filename := "birthday_001.txt"
-	// convert into "Birthday - 1 of 4.txt"
-	newName, err := match(filename)
+	// filename := "birthday_001.txt"
+	// // convert into "Birthday - 1 of 4.txt"
+	// newName, err := match(filename)
+	// if err != nil {
+	// 	fmt.Println("no match")
+	// 	os.Exit(0)
+	// }
+	// fmt.Println(newName)
+	dir := "./sample"
+	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		fmt.Println("no match")
-		os.Exit(0)
+		panic(err)
 	}
-	fmt.Println(newName)
+	count := 0
+	var toRename []string
+
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Println("Dir: ", file.Name())
+		} else {
+			_, err := match(file.Name(), 0)
+			if err == nil {
+				count++
+				toRename = append(toRename, file.Name())
+			}
+		}
+	}
+	for _, origFilename := range toRename {
+		origPath := filepath.Join(dir, origFilename)
+		newFilename, err := match(origFilename, count)
+		if err != nil {
+			panic(err)
+		}
+		newPath := filepath.Join(dir, newFilename)
+		fmt.Printf("mv %s => %s\n", origPath, newPath)
+	}
+	// origPath := fmt.Sprintf("%s/%s")
+	// newPath := fmt.Sprintf("%s/%s")
 }
 
 // match returns the new filename or an error if filename didn't match our pattern
-func match(filename string) (string, error) {
+func match(filename string, total int) (string, error) {
 	// split into "birthday", "001", "txt"
 	pieces := strings.Split(filename, ".")
 	ext := pieces[len(pieces)-1]
@@ -31,5 +62,5 @@ func match(filename string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("%s didn't match our pattern", filename)
 	}
-	return fmt.Sprintf("%s - %d.%s", strings.Title(name), number, ext), nil
+	return fmt.Sprintf("%s - %d of %d.%s", strings.Title(name), number, total, ext), nil
 }
