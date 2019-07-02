9170530c8065232fe672b9c2a2a07df5bda28d83
Zach North <zrnorth@gmail.com>
2019-07-01 18:14:13 -0700

    Problem 17, added encrypt/decrypt from std lib functions

---
+++ gopher-17/encrypt/cipher.go
@@ -0,0 +1,67 @@
+package encrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+)
+
+// Encrypt will take in a key and some plaintext and return an encrypted
+// version of the plaintext in hex form
+func Encrypt(key, plaintext string) (string, error) {
+	block, err := newCipherBlock(key)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plaintext))
+
+	// It's important to remember that ciphertexts must be authenticated
+	// (i.e. by using crypto/hmac) as well as being encrypted in order to
+	// be secure.
+	return fmt.Sprintf("%x", ciphertext), nil
+}
+
+// Decrypt will take in a key and hexed representation of the ciphertext
+// and decrypt it
+func Decrypt(key, cipherHex string) (string, error) {
+	block, err := newCipherBlock(key)
+	if err != nil {
+		return "", err
+	}
+	ciphertext, err := hex.DecodeString(cipherHex)
+	if err != nil {
+		return "", err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return "", errors.New("encrypt: ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+	return string(ciphertext), nil
+}
+
+func newCipherBlock(key string) (cipher.Block, error) {
+	hasher := md5.New()
+	fmt.Fprint(hasher, key)
+	cipherKey := hasher.Sum(nil)
+	return aes.NewCipher(cipherKey)
+}
